0825c5f5ee609b699d15294b5a2c405fb5993db6
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 12:46:03 -0300

    backend: using cors()

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes'); // when you are importing a file inside your project, you need to use ./ and specify the full path to be recognized
 const server = express();
 
@@ -7,6 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-akxwi.mongodb.net/o
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes); // you use .use, when you want to apply some config from another file
 
