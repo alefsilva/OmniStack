7b0fa51075dcc18100a97ef0a66623f215dec97c
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 09:41:05 -0300

    backend: connection with MongoDB Atlas database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes'); // when you are importing a file inside your project, you need to use ./ and specify the full path to be recognized
 const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-akxwi.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes); // you use .use, when you want to apply some config from another file
