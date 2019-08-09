8b3972615cc1065f4c5528aa573576b4d714d04e
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 12:13:36 -0300

    backend: deslike service

---
+++ backend/src/controllers/DeslikeController.js
@@ -0,0 +1,21 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId); // dev that is receiving a like
+    console.log("param devId: ", devId);
+    console.log("headers user: ", user);
+
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+
+    loggedDev.deslikes.push(targetDev._id); //add
+    await loggedDev.save(); // and save on database
+
+    return res.json(loggedDev);
+  }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const DevController = require("./controllers/DevController");
 const LikeController = require("./controllers/LikeController");
+const DeslikeController = require("./controllers/DeslikeController");
 
 const routes = express.Router();
 
@@ -14,5 +15,6 @@ routes.get("/", (req, res) => {
 // });
 routes.post("/devs", DevController.store);
 routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/deslikes", DeslikeController.store);
 
 module.exports = routes;
