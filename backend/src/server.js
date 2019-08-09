5f796bf86917ded1f1416202b3767b84d565e48a
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 08:15:25 -0300

    backend - refactoring: created the routes file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const routes = require('./routes'); // when you are importing a file inside your project, you need to use ./ and specify the full path to be recognized
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.json({ message: `Hello ${req.query.name}` });
-});
+server.use(routes); // you use .use, when you want to apply some config from another file
 
 server.listen(7777);
