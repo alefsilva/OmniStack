98c9ee0d29323ea51dd0053eed40de9ce7031058
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 08:58:03 -0300

    backend - post route: try get body

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,4 +5,9 @@ routes.get("/", (req, res) => {
   return res.json({ message: `Hello ${req.query.name}` });
 });
 
+routes.post("/devs", (req, res) => {
+    console.log('body:', req.body); // by default it will return 'body: undefined' because the express don't recognize the json format automatically
+    return res.json({ oks: true });
+});
+
 module.exports = routes;
