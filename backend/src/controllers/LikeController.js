abcbdbf0f15ec150e442e228723de789674baeb5
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 11:59:44 -0300

    backend: validation to check if the user exist

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,12 +1,18 @@
 const Dev = require("../models/Dev");
 
 module.exports = {
-  store(req, res) {
-    const { devId} = req.params;
+  async store(req, res) {
     const { user } = req.headers;
-    console.log('param devId: ', devId);
-    console.log('headers user: ', user);
+    const { devId } = req.params;
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId); // dev that is receiving a like
+    console.log("param devId: ", devId);
+    console.log("headers user: ", user);
 
-    return res.json({ok: true});
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+
+    return res.json({ ok: true });
   }
 };
