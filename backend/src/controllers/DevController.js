f271f4e7e5310e87f68dd1de53860cdf8458e21a
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 11:07:09 -0300

    backend: created a new user using the return data of github api

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,9 +1,19 @@
-const axios = require('axios');
+const axios = require("axios");
+const Dev = require("../models/Dev");
 
 module.exports = {
-    async store(req, res) {
-        const { username } = req.body;
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        return res.json(response.data);// the axios return the data inside data object
-    }
+  async store(req, res) {
+    const { username } = req.body;
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+    const { name, bio, avatar_url: avatar } = response.data;
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    });
+    return res.json(dev); // the axios return the data inside data object
+  }
 };
