26b0b5a46e955a592e5e2ec0cf56aa9ec577ee35
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 10:07:11 -0300

    backend - routes: store new dev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const DevController = require('./controllers/DevController');
+
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
   return res.json({ message: `Hello ${req.query.name}` });
 });
 
-routes.post("/devs", (req, res) => {
-  console.log("body:", req.body); // by default it will return 'body: undefined' because the express don't recognize the json format automatically
-  return res.json(Object.assign(req.body, { ok: true }));
-});
+// routes.post("/devs", (req, res) => {
+//   console.log("body:", req.body); // by default it will return 'body: undefined' because the express don't recognize the json format automatically
+//   return res.json(Object.assign(req.body, { ok: true }));
+// });
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
