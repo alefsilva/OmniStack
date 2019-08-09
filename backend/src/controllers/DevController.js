372ffd0216d6f3b267816e624aec1467f23755b9
Alef Silva <alef.silva@paketa.com.br>
2019-08-09 12:36:34 -0300

    backend: created the index route to get all users that the current user don't react yet

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,20 @@ const axios = require("axios");
 const Dev = require("../models/Dev");
 
 module.exports = {
+  async index(req, res) {
+    const { user_id } = req.headers;
+    console.log('user_id: ', user_id);
+    const loggedDev = await Dev.findById(user_id);
+    const users  = await Dev.find({
+      $and: [
+        { _id: { $ne: user_id }}, // filter $ne(not equal) the user_id
+        { _id: { $nin: loggedDev.likes }}, // filter to avoid all user that already have like with $nin(not in)
+        { _id: { $nin: loggedDev.deslikes }}, // the same filter as above for deslikes
+      ]
+    });
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
     const userExist = await Dev.findOne({ user: username });
