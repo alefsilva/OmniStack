1852ad8c0832f4cf8ffec3121de817de35bbbfca
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-12 15:46:48 -0300

    frontend: created component Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Login from "./pages/login/login";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello Rockeseat</p>
+      <Login />
     </div>
   );
 }
---
+++ frontend/src/pages/login/login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Logo from "../../assets/logo.svg";
+
+export default function Login() {
+  return <img src={Logo} alt="Tindev" />;
+}
