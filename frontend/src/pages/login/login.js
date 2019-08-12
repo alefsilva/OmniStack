be32beaa7d04acabc53f6d6f867e03200f949ed1
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-12 16:41:05 -0300

    frontend - useState: simulating the two way data binding

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 import Logo from "../../assets/logo.svg";
 
 export default function Login() {
+  const [userName, setUserName] = useState("");
+
+  function _handleSubmit(event){
+    event.preventDefault();
+    console.log('userName ==', userName);
+  }
+
   return (
     <div className="wrap-login-page">
-      <form>
+      <form onSubmit={_handleSubmit}>
         <img src={Logo} alt="Tindev" />
-        <input placeholder="Digite o seu usuário do GitHub" />
+        <input
+          placeholder="Digite o seu usuário do GitHub"
+          value={userName}
+          onChange={event => setUserName(event.target.value)} />
         <button type="submit">Enviar</button>
       </form>
     </div>
