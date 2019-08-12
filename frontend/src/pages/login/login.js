02da9d95f37d1cdb2a2e9f8d8f365e7c205c90e8
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-12 16:45:02 -0300

    frontend - props: redirecting to main page

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -3,12 +3,13 @@ import "./Login.css";
 
 import Logo from "../../assets/logo.svg";
 
-export default function Login() {
+export default function Login(props) { // destructuring mode: { history }
   const [userName, setUserName] = useState("");
 
   function _handleSubmit(event){
     event.preventDefault();
     console.log('userName ==', userName);
+    props.history.push('/main');
   }
 
   return (
