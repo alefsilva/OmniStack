7bedb8c7016e1f88f9e4a0d2ece9fc8be6ae7bd0
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-12 16:12:51 -0300

    frontend - Login: layout

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -6,7 +6,11 @@ import Logo from "../../assets/logo.svg";
 export default function Login() {
   return (
     <div className="wrap-login-page">
-      <img src={Logo} alt="Tindev" />
+      <form>
+        <img src={Logo} alt="Tindev" />
+        <input placeholder="Digite o seu usuário do GitHub" />
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
