d663e369a54de3b6c1fd4e810af22504795278ab
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-17 17:52:49 -0300

    frontend: using useEffect to listen routeId changes to get data

--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../../assets/logo.svg";
 import like from "../../assets/like.svg";
 import dislike from "../../assets/dislike.svg";
 import "./Main.css";
 
+import api from '../../services/Api';
+
 export default function Main(props) {
+  useEffect(() => {
+    (async function loadUsers() {
+      console.log('props.match.params.routeId', props.match.params.routeId);
+      const response = await api.get('/devs', {
+        headers: {
+          user_id: props.match.params.routeId
+        }
+      });
+      console.log('response.data', response.data);
+    })(); // it will execute this function automatically
+
+    //loadUsers();
+  }, [props.match.params.routeId]); // if the second parameter was [], the useEffect will be run once
+
   return (
     <div className="wrap-main">
       <h1>routeId: {props.match.params.routeId}</h1>
