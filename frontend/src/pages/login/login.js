49a6d352ef0f6fcb495d923d9759bbff84277b7f
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-17 17:16:07 -0300

    frontend: refactoring

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -4,17 +4,18 @@ import "./Login.css";
 import Logo from "../../assets/logo.svg";
 import Api from "../../services/Api";
 
-export default function Login(props) { // destructuring mode: { history }
+export default function Login(props) {
+  // destructuring mode: { history }
   const [userName, setUserName] = useState("");
 
-  async function _handleSubmit(event){
+  async function _handleSubmit(event) {
     event.preventDefault();
-    console.log('userName ==', userName);
+    console.log("userName ==", userName);
 
-    const response = await Api.post('/devs', {
+    const response = await Api.post("/devs", {
       username: userName
     });
-    console.log('response.data ==', response.data);
+    console.log("response.data ==", response.data);
     const { _id: id } = response.data;
     props.history.push(`/main/${id}`);
   }
@@ -26,7 +27,8 @@ export default function Login(props) { // destructuring mode: { history }
         <input
           placeholder="Digite o seu usuário do GitHub"
           value={userName}
-          onChange={event => setUserName(event.target.value)} />
+          onChange={event => setUserName(event.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
