72c3671268e616b940608bf11dcc6bf870a258e6
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-19 22:14:54 -0300

    app: StyleSheet

--- app/App.js
+++ app/App.js
@@ -7,8 +7,29 @@
  */
 
 import React from 'react'
-import {Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 export default function App() {
-  return <Text>Hello World</Text>
+  return (
+    <View style={styles.container}>
+      {/*css inline {flex: 1, backgroundColor: '#7159c1'}*/}
+      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    flexDirection: 'row', // by default, the flexDirection is column. Then, the justifyContent and alignItems are opposite
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 20,
+  },
+})
