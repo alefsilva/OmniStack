3db6d9e4c1a82e5b26455ad7a1450fc6a5caf580
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-22 21:39:33 -0300

    app: created input layout

--- app/src/pages/login/Login.js
+++ app/src/pages/login/Login.js
@@ -7,21 +7,37 @@ export default function Login() {
   return (
     <View style={styles.wrapContainer}>
       <Image source={Logo} />
-      <Text>{/*
-            * the react import the 2x, 3x automatically
-            * don't have one element form
-    */}</Text>
-      <TextInput placeholder="Digie seu usuário no GitHub" />
+      <Text>
+        {/*
+         * the react import the 2x, 3x automatically
+         * don't have one element form
+         */}
+      </Text>
+      <TextInput
+        placeholder="Digite seu usuário no GitHub"
+        placeHolderTextColor="#999"
+        style={styles.input}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapContainer: {
-      flex: 1,
-      backgroundColor: '#f5f5f5',
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 30
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
   },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15 // padding: 0 15px;
+  }
 });
