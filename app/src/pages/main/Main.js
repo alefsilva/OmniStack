d14eafbbfb2d81fd425f72715fb0103cdf00fc7b
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-25 06:19:07 -0300

    app - main: using styled-components

--- app/src/pages/main/Main.js
+++ app/src/pages/main/Main.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {SafeAreaView, Text, Image} from 'react-native';
+import styled from 'styled-components/native';
 
 import Logo from '../../assets/logo.png';
 
+const StyledSafeAreaView = styled(SafeAreaView)`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  background-color: papayawhip;
+`;
+
 export default function Main() {
   return (
-    <SafeAreaView>
+    <StyledSafeAreaView>
       <Image source={Logo} />
-    </SafeAreaView>
+    </StyledSafeAreaView>
   );
 }
