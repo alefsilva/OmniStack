f9eabc28314b99c871fd9b96c2a178f3bd441e51
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-26 20:34:09 -0300

    backend: setting websocket connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,15 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const routes = require("./routes"); // when you are importing a file inside your project, you need to use ./ and specify the full path to be recognized
-const server = express();
+const app = express();
+// we'll extract the http server from inside the express to join with a ws server or websocket server
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+  // each new connection has a unique id that is socket.id
+  console.log("New connection", socket.id);
+});
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0-akxwi.mongodb.net/omnistack8?retryWrites=true&w=majority",
@@ -12,8 +20,8 @@ mongoose.connect(
   }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes); // you use .use, when you want to apply some config from another file
+app.use(cors());
+app.use(express.json());
+app.use(routes); // you use .use, when you want to apply some config from another file
 
 server.listen(7777);
