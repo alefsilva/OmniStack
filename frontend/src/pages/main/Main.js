3bc8adc72f95f4c73c1efd9766412f8c3a98ed73
Alef Silva <alefsilva.front.ux@gmail.com>
2019-08-31 17:33:20 -0300

    frontend: refactoring

--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from "../../assets/logo.svg";
-import like from "../../assets/like.svg";
-import dislike from "../../assets/dislike.svg";
-import "./Main.css";
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+import './Main.css';
 
-import api from "../../services/Api";
+import api from '../../services/Api';
 
 export default function Main(props) {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     (async function loadUsers() {
-      console.log("props.match.params.routeId", props.match.params.routeId);
-      const response = await api.get("/devs", {
+      console.log('props.match.params.routeId', props.match.params.routeId);
+      const response = await api.get('/devs', {
         headers: {
           user_id: props.match.params.routeId
         }
       });
-      console.log("response.data", response.data);
+      console.log('response.data', response.data);
       setUsers(response.data);
     })(); // it will execute this function automatically
 
@@ -27,7 +27,7 @@ export default function Main(props) {
   }, [props.match.params.routeId]); // if the second parameter was [], the useEffect will be run once
 
   async function handleLike(id) {
-    console.log("like", id);
+    console.log('like', id);
 
     await api.post(`devs/${id}/likes`, null, {
       headers: {
@@ -38,7 +38,7 @@ export default function Main(props) {
   }
 
   async function handleDislike(id) {
-    console.log("dislike", id);
+    console.log('dislike', id);
     await api.post(`devs/${id}/dislikes`, null, {
       headers: {
         user_id: props.match.params.routeId
