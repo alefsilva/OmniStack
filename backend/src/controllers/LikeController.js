fcb853504808b31e1c01fa3c1b334277737fc197
Alef Silva <alefsilva.front.ux@gmail.com>
2019-09-09 21:17:32 -0300

    backend: refactoring

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,4 +1,4 @@
-const Dev = require("../models/Dev");
+const Dev = require('../models/Dev');
 
 module.exports = {
   async store(req, res) {
@@ -9,21 +9,19 @@ module.exports = {
     const { devId } = req.params;
     const loggedDev = await Dev.findById(user_id);
     const targetDev = await Dev.findById(devId); // dev that is receiving a like
-    console.log("param devId: ", devId);
-    console.log("headers user_id: ", user_id);
+    console.log('param devId: ', devId);
+    console.log('headers user_id: ', user_id);
 
-    if (!targetDev) return res.status(400).json({ error: "Dev not exists" });
+    if (!targetDev) return res.status(400).json({ error: 'Dev not exists' });
 
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log("DEU MATCH");
+      console.log('DEU MATCH');
       const loggedSocket = req.connectedUsers[user_id];
       const targetSocket = req.connectedUsers[devId];
 
-      if (loggedSocket)
-        req.io.to(loggedSocket).emit('match', targetDev);
+      if (loggedSocket) req.io.to(loggedSocket).emit('match', targetDev);
 
-      if (targetSocket)
-        req.io.to(targetSocket).emit('match', loggedDev);
+      if (targetSocket) req.io.to(targetSocket).emit('match', loggedDev);
     }
 
     loggedDev.likes.push(targetDev._id); //add
