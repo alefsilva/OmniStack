5148152c00752cb3b64bf0ef19af589bd4c58267
Alef Silva <alefsilva.front.ux@gmail.com>
2019-09-14 17:56:32 -0300

    app - warnings: using YellowBox settings to ignore some warnings

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import { YellowBox } from 'react-native';
 import Routes from './routes';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection',
+  'Node of type rule not supported as an inline style'
+]);
 
 export default function App() {
   return <Routes />;
